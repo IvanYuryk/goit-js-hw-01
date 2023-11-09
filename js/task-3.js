1ada7d1a50e688b69906d768c451fc7a14fc87aa
IvanYuryk <143449289+IvanYuryk@users.noreply.github.com>
2023-11-09 17:42:13 +0100

    v4

--- js/task-3.js
+++ js/task-3.js
@@ -1,10 +1,8 @@
 function getElementWidth(content, padding, border){
 const contentWidth = parseFloat(content);
   const paddingWidth = parseFloat(padding);
-    const borderWidth = parseFloat(border);
-    const getElementWidth = contentWidth + 2 * paddingWidth + 2 * borderWidth;
-
-  return getElementWidth;
+   const borderWidth = parseFloat(border);
+      return contentWidth + 2 * paddingWidth + 2 * borderWidth;
 }
 
 console.log(getElementWidth("50px", "8px", "4px")); // 74
